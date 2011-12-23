09ea45a6ed430e4c52d09ebe41c640026a35a3fc
Rob Beane <robbeane@gmail.com>
2011-12-23 12:16:34 -0500

    Ace: remove calls to cm vendor

--- cm.mk
+++ cm.mk
@@ -3,10 +3,10 @@ $(call inherit-product, device/htc/ace/full_ace.mk)
 PRODUCT_RELEASE_NAME := DHD
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+#$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+#$(call inherit-product, vendor/cm/config/gsm.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ace BUILD_ID=GRI40 BUILD_FINGERPRINT=htc_wwe/htc_ace/ace:2.3.3/GRI40/87995:user/release-keys PRIVATE_BUILD_DESC="2.50.405.2 CL87995 release-keys"
 
